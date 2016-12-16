5b9d9453718aa2be07240670c716588c15669387
salzh <zhongxiang721@gmail.com>
2016-12-16 03:06:48 -0500

    Add daemon to monitor e911

---
+++ monitor_e911.pl
@@ -0,0 +1,89 @@
+#!/usr/bin/perl
+use LWP::Simple;
+use File::Tail;
+
+$out = `ps aux | grep $0 | grep -v 'grep ' | wc -l`;
+chomp $out;
+
+use JSON; # to install, # sudo cpan JSON
+$json_engine	= JSON->new->allow_nonref;
+
+if ($out > 1) {
+    warn "another $0 is already running, quit! ...";
+    exit 0;
+}
+
+my $now_file = "/tmp/fail2ban_now.log";
+my $lock_file = '/tmp/fail2ban_restart.lock';
+
+my $txt = `cat /etc/e911.conf`;
+    
+my %config = ();
+for (split /\n/, $txt) {
+    my ($key, $val)	= split /=/, $_, 2;
+    
+    if ($key) {
+        $config{$key} = $val;
+        warn "$key=$val\n";
+    }
+}
+
+
+my $file = "/var/log/2600hz/kazoo.log";
+
+
+#{"data":{"state":"in_service","used_by":"callflow","state":"in_service","used_by":"callflow","numbers":["+18183092674"],"ui_metadata":{"version":"3.22-34","ui":"monster-ui","origin":"common"},"id":"+18183092674","cnam":{"display_name":"Velantro"},"dash_e911":{"postal_code":"91204","street_address":"730 S Central Ave","extended_address":" #211, Glendale","locality":"Glendale","region":"California"},"_read_only":{"created":63648768639,"modified":63649089017,"state":"in_service","used_by":"callflow"}},"revision":"undefined","request_id":"71787e0a76c587d059effa8b064be4cd","status":"success","auth_token":"f99a5e06bca1fb15bc6380e4879ee155"}
+$size = 0;
+
+my $file = File::Tail->new(name => "/var/log/2600hz/kazoo.log", tail => -1, interval => 2);
+while (defined($line = $file->read)) {	
+
+# warn $line, "\n";
+#2014-10-27 19:36:47,003 fail2ban.actions: WARNING [freeswitch-tcp] Ban 71.95.176.58
+	if ($line =~ / pull response content: (.+)$/) {
+	    my $json = $1;
+	    next unless $line =~ /dash_e911/;
+	    warn "[e911-json] $json\n";
+
+	    %options = &Json2Hash($json);
+	    
+	   
+	    $did = $options{data}{id}; $did =~ s/\D//g; $did =~ s/^1//g;
+	    next unless $did =~ /^1?\d{10}$/;
+			
+			warn "invalid: $json\n";
+	    $name = $options{data}{cnam}{display_name} || 'Velantro';
+	    $address =  $options{data}{dash_e911}{street_address};
+	    $city = $options{data}{dash_e911}{locality};
+	    $state = $options{data}{dash_e911}{region};
+	    $zip = $options{data}{dash_e911}{postal_code};
+	    
+	    
+	    $url = "http://api.vitelity.net/api.php?login=$config{api_user}&pass=$config{api_pass}&cmd=e911send&did=$did&name=$name&address=$address&city=$city&state=$state&zip=$zip";
+	    
+	    warn "url: $url\n";
+	    
+	    $res = get $url;
+	    warn "response: $res\n";			        
+				
+	}
+}
+
+sub Json2Hash(){
+	local($json_plain) = @_;
+	local(%json_data);
+	my %json_data = ();
+
+	
+	
+	if ($json_plain ne "") {
+		local $@;
+		eval {
+			$json_data_reference	= $json_engine->decode($json_plain);
+		};
+		
+		if ($@) {warn $@}
+		%json_data			= %{$json_data_reference};
+	}
+	return %json_data;
+}
